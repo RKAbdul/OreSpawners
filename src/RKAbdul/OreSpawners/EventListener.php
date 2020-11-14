ca2014554ca3d4f274bdf81a8364ae732d966d57
RKAbdul <64411354+RKAbdul@users.noreply.github.com>
2020-11-14 17:55:37 +0100

    fixed some posssible bugs

--- src/RKAbdul/OreSpawners/EventListener.php
+++ src/RKAbdul/OreSpawners/EventListener.php
@@ -4,46 +4,46 @@ declare(strict_types=1);
 
 namespace RKAbdul\OreSpawners;
 
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\event\Listener;
-use pocketmine\event\block\BlockUpdateEvent;
+use DenielWorld\EzTiles\data\TileInfo;
+use DenielWorld\EzTiles\tile\SimpleTile;
+use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockUpdateEvent;
+use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\item\Item;
-use pocketmine\block\Block;
-use pocketmine\scheduler\ClosureTask;
-use pocketmine\math\Vector3;
+use pocketmine\tile\Tile;
 use pocketmine\level\sound\FizzSound;
-use pocketmine\utils\Config;
-use DenielWorld\EzTiles\tile\SimpleTile;
-use DenielWorld\EzTiles\data\TileInfo;
+use pocketmine\math\Vector3;
+use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat as TF;
 
-class EventListener implements Listener{
+class EventListener implements Listener
+{
 
     /**
      * @var Main
      */
     private $plugin;
-    
+
     private $cfg;
-    
-    public function __construct(Main $plugin){
+
+    public function __construct(Main $plugin)
+    {
         $this->plugin = $plugin;
         $this->cfg = $this->plugin->getConfig()->getAll();
     }
-	
-	public function onBlockUpdate(BlockUpdateEvent $event){
+
+    public function onBlockUpdate(BlockUpdateEvent $event)
+    {
         $block = $event->getBlock();
         $bbelow = $block->getLevel()->getBlock($event->getBlock()->floor()->down(1));
         $blocks = [];
-        
-        foreach(array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
+
+        foreach (array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
             array_push($blocks, $blockID);
-        };
-        
+        }
+
         if (in_array($bbelow->getId(), $blocks)) {
             $tile = $event->getBlock()->getLevel()->getTile($bbelow);
             if (!$tile instanceof SimpleTile) return;
@@ -52,25 +52,73 @@ class EventListener implements Listener{
             if (!$event->isCancelled()) {
                 $event->setCancelled(true);
                 if ($event->getBlock()->getId() == $ore->getId()) return;
-                $this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask( function (int $currentTick) use ($event, $ore): void {
-                    if ($event->getBlock()->getLevel() !== null){
+                $this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($event, $ore): void {
+                    if ($event->getBlock()->getLevel() !== null) {
                         $event->getBlock()->getLevel()->setBlock($event->getBlock()->floor(), $ore, false, true);
-			            if ($this->cfg["fizz-sound"] == true) $event->getBlock()->getLevel()->addSound(new FizzSound($event->getBlock()->asVector3()));
+                        if ($this->cfg["fizz-sound"] == true) $event->getBlock()->getLevel()->addSound(new FizzSound($event->getBlock()->asVector3()));
                     }
                 }), intval($delay));
             }
         }
-	}
-	
-	public function onBlockPlace(BlockPlaceEvent $event) {
-	    $block = $event->getBlock();
-	    $item = $event->getItem();
-	    $blocks = [];
-        
-        foreach(array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
+    }
+
+    public function checkBlock(Block $bbelow)
+    {
+        $bbid = $bbelow->getId();
+        $coalid = intval($this->cfg["ore-generator-blocks"]["coal"]);
+        $ironid = intval($this->cfg["ore-generator-blocks"]["iron"]);
+        $goldid = intval($this->cfg["ore-generator-blocks"]["gold"]);
+        $diamondid = intval($this->cfg["ore-generator-blocks"]["diamond"]);
+        $emeraldid = intval($this->cfg["ore-generator-blocks"]["emerald"]);
+        $lapizid = intval($this->cfg["ore-generator-blocks"]["lapis"]);
+        $redstoneid = intval($this->cfg["ore-generator-blocks"]["redstone"]);
+        switch ($bbid) {
+            case $coalid:
+                $ore = Block::get(Block::COAL_ORE);
+                break;
+            case $ironid:
+                $ore = Block::get(Block::IRON_ORE);
+                break;
+            case $goldid:
+                $ore = Block::get(Block::GOLD_ORE);
+                break;
+            case $diamondid:
+                $ore = Block::get(Block::DIAMOND_ORE);
+                break;
+            case $emeraldid:
+                $ore = Block::get(Block::EMERALD_ORE);
+                break;
+            case $lapizid:
+                $ore = Block::get(Block::LAPIS_ORE);
+                break;
+            case $redstoneid:
+                $ore = Block::get(Block::REDSTONE_ORE);
+                break;
+        }
+        if (isset($ore)) {
+            return $ore;
+        }
+        return false;
+    }
+
+    public function getDelay(Block $block)
+    {
+        $tile = $block->getLevel()->getTile($block->asVector3());
+        $stacked = $tile->getData("stacked")->getValue();
+        $base = intval($this->cfg["base-delay"]);
+        return ($base / $stacked) * 20;
+    }
+
+    public function onBlockPlace(BlockPlaceEvent $event)
+    {
+        $block = $event->getBlock();
+        $item = $event->getItem();
+        $blocks = [];
+
+        foreach (array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
             array_push($blocks, $blockID);
-        };
-        
+        }
+
         if (in_array($block->getId(), $blocks)) {
             if ($item->getNamedTag()->hasTag("orespawner")) {
                 $tile = $event->getPlayer()->getLevel()->getTile($event->getBlock()->asVector3());
@@ -80,55 +128,62 @@ class EventListener implements Listener{
                 }
             }
         }
-	}
-    
-    public function onPlayerInteract(PlayerInteractEvent $event) {
-        if($this->cfg["stacking"] == false || $event->isCancelled()) return;
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event): bool
+    {
+        if ($this->cfg["stacking"] == false || $event->isCancelled()) return false;
         $item = $event->getItem();
         $player = $event->getPlayer();
         $blocks = [];
-        foreach(array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID){
+        foreach (array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
             array_push($blocks, $blockID);
-        };
-        if (in_array($event->getBlock()->getId(), $blocks)) {
-            if (!$item->getNamedTag()->hasTag("orespawner")) return;
-            $tile = $event->getPlayer()->getLevel()->getTile($event->getBlock());
-            if (!$tile instanceof SimpleTile) return;
-            if($player->getGamemode() == 1) return $player->sendMessage(TF::RED . "You can only use stacking system in survival");
-            $stacked = $tile instanceof SimpleTile ? $tile->getData("stacked")->getValue() : 1;
-            
-            if (!in_array($item->getId(), $blocks)) return $player->sendMessage("§aThere are currently " . TF::YELLOW . $stacked. " §astacked OreSpawners");
-            if ($event->getBlock()->getId() != $item->getId()) return $player->sendMessage("§cPlease hold the right type of OreSpawner to stack");
-            if ($stacked >= intval($this->cfg["max"])) return $player->sendMessage(str_replace("&", "§", $this->cfg["limit-reached"] ?? "&cYou can't stack anymore orespawners, you have reached the limit"));
-            $event->setCancelled(true);
-
-            $tile->setData("stacked", $stacked + 1);
-            $item->setCount($item->getCount() - 1);
-            $player->getInventory()->setItem($player->getInventory()->getHeldItemIndex(), $item);
-            $player->sendMessage(str_replace("&", "§", $this->cfg["gen-added"] ?? "&aSuccessfully stacked a orespawner"));
         }
+        if (in_array($event->getBlock()->getId(), $blocks)) {
+            if (!$item->getNamedTag()->hasTag("orespawner")) return false;
+            $tile = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+            if ($tile instanceof SimpleTile) {
+                if (!$player->getGamemode() == 1) {
+                    $stacked = $tile->getData("stacked")->getValue();
+                    if (in_array($item->getId(), $blocks)) {
+                        if (!$event->getBlock()->getId() != $item->getId()) {
+                            if (!$stacked >= intval($this->cfg["max"])) {
+                                $event->setCancelled(true);
+                                $tile->setData("stacked", $stacked + 1);
+                                $item->setCount($item->getCount() - 1);
+                                $player->getInventory()->setItem($player->getInventory()->getHeldItemIndex(), $item);
+                                $player->sendMessage(str_replace("&", "§", $this->cfg["gen-added"] ?? "&aSuccessfully stacked a orespawner"));
+                            }
+                            $player->sendMessage(str_replace("&", "§", $this->cfg["limit-reached"] ?? "&cYou can't stack anymore orespawners, you have reached the limit"));
+                        }
+                        $player->sendMessage("§cPlease hold the right type of OreSpawner to stack");
+                        return false;
+                    }
+                    $player->sendMessage("§aThere are currently " . TF::YELLOW . $stacked . " §astacked OreSpawners");
+                    return false;
+                }
+                $player->sendMessage(TF::RED . "You can only using stacking system in Survival.");
+                return false;
+            }
+            return false;
+        }
+        return false;
     }
-    
-    public function getDelay(Block $block) {
-        $tile = $block->getLevel()->getTile($block->asVector3());
-        $stacked = $tile->getData("stacked")->getValue();
-        $base = intval($this->cfg["base-delay"]);
-        $delay = ($base / $stacked) * 20;
-        return $delay;
+
+    public function getTile(Vector3 $pos): ?Tile
+    {
+        return $this->getTileAt((int)floor($pos->x), (int)floor($pos->y), (int)floor($pos->z));
     }
-    
-    public function getTile(Vector3 $pos) : ?Tile {
-	    return $this->getTileAt((int) floor($pos->x), (int) floor($pos->y), (int) floor($pos->z));
-    }
-	
-    public function onBlockBreak(BlockBreakEvent $event){
+
+    public function onBlockBreak(BlockBreakEvent $event)
+    {
         $player = $event->getPlayer();
         $block = $event->getBlock();
         $bbelow = $block->getLevel()->getBlock($event->getBlock()->floor()->down(1));
         $blocks = [];
-        foreach(array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID){
+        foreach (array_values($this->plugin->getConfig()->get("ore-generator-blocks")) as $blockID) {
             array_push($blocks, $blockID);
-        };
+        }
         if ($event->isCancelled()) return;
         if (in_array($event->getBlock()->getId(), $blocks)) {
             $tile = $event->getBlock()->getLevel()->getTile($block);
@@ -139,81 +194,50 @@ class EventListener implements Listener{
             $orespawner = $this->plugin->createOreSpawner($type, $count);
             $drops = array();
             $drops[] = $orespawner;
-	        $event->setDrops($drops);
+            $event->setDrops($drops);
         } else if (in_array($bbelow->getId(), $blocks)) {
             if ($this->cfg["drop-xp"] == false) {
                 $event->setXpDropAmount(0);
             }
         }
     }
-    
-    public function checkBlock(Block $bbelow) {
+
+    public function checkSpawner(Block $bbelow)
+    {
         $bbid = $bbelow->getId();
         $coalid = intval($this->cfg["ore-generator-blocks"]["coal"]);
-	    $ironid = intval($this->cfg["ore-generator-blocks"]["iron"]);
-	    $goldid = intval($this->cfg["ore-generator-blocks"]["gold"]);
-	    $diamondid = intval($this->cfg["ore-generator-blocks"]["diamond"]);
-	    $emeraldid = intval($this->cfg["ore-generator-blocks"]["emerald"]);
-	    $lapizid = intval($this->cfg["ore-generator-blocks"]["lapis"]);
-	    $redstoneid = intval($this->cfg["ore-generator-blocks"]["redstone"]);
-	    switch ($bbid) {
-	        case $coalid:
-	            $ore = Block::get(Block::COAL_ORE);
-	            break;
-	        case $ironid:
-	            $ore = Block::get(Block::IRON_ORE);
-	            break;
-	        case $goldid:
-	            $ore = Block::get(Block::GOLD_ORE);
-	            break;
-	        case $diamondid:
-	            $ore = Block::get(Block::DIAMOND_ORE);
-	            break;
-	        case $emeraldid:
-	            $ore = Block::get(Block::EMERALD_ORE);
-	            break;
-	        case $lapizid:
-	            $ore = Block::get(Block::LAPIS_ORE);
-	            break;
-	        case $redstoneid:
-	            $ore = Block::get(Block::REDSTONE_ORE);
-	            break;
-	    }
-	    return $ore;
-    }
-    
-    public function checkSpawner(Block $bbelow) {
-        $bbid = $bbelow->getId();
-        $coalid = intval($this->cfg["ore-generator-blocks"]["coal"]);
-	    $ironid = intval($this->cfg["ore-generator-blocks"]["iron"]);
-	    $goldid = intval($this->cfg["ore-generator-blocks"]["gold"]);
-	    $diamondid = intval($this->cfg["ore-generator-blocks"]["diamond"]);
-	    $emeraldid = intval($this->cfg["ore-generator-blocks"]["emerald"]);
-	    $lapizid = intval($this->cfg["ore-generator-blocks"]["lapis"]);
-	    $redstoneid = intval($this->cfg["ore-generator-blocks"]["redstone"]);
-	    switch ($bbid) {
-	        case $coalid:
-	            $ore = "coal";
-	            break;
-	        case $ironid:
-	            $ore = "iron";
-	            break;
-	        case $goldid:
-	            $ore = "gold";
-	            break;
-	        case $diamondid:
-	            $ore = "diamond";
-	            break;
-	        case $emeraldid:
-	            $ore = "emerald";
-	            break;
-	        case $lapizid:
-	            $ore = "lapis";
-	            break;
-	        case $redstoneid:
-	            $ore = "redstone";
-	            break;
-	    }
-	    return $ore;
+        $ironid = intval($this->cfg["ore-generator-blocks"]["iron"]);
+        $goldid = intval($this->cfg["ore-generator-blocks"]["gold"]);
+        $diamondid = intval($this->cfg["ore-generator-blocks"]["diamond"]);
+        $emeraldid = intval($this->cfg["ore-generator-blocks"]["emerald"]);
+        $lapizid = intval($this->cfg["ore-generator-blocks"]["lapis"]);
+        $redstoneid = intval($this->cfg["ore-generator-blocks"]["redstone"]);
+        switch ($bbid) {
+            case $coalid:
+                $ore = "coal";
+                break;
+            case $ironid:
+                $ore = "iron";
+                break;
+            case $goldid:
+                $ore = "gold";
+                break;
+            case $diamondid:
+                $ore = "diamond";
+                break;
+            case $emeraldid:
+                $ore = "emerald";
+                break;
+            case $lapizid:
+                $ore = "lapis";
+                break;
+            case $redstoneid:
+                $ore = "redstone";
+                break;
+        }
+        if (isset($ore)) {
+            return $ore;
+        }
+        return false;
     }
 }
--- src/RKAbdul/OreSpawners/Main.php
+++ src/RKAbdul/OreSpawners/Main.php
@@ -4,84 +4,84 @@ declare(strict_types=1);
 
 namespace RKAbdul\OreSpawners;
 
-use function mkdir;
-use function file_exists;
-
-use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
-use pocketmine\Server;
-use pocketmine\Player;
-use pocketmine\utils\TextFormat as TF;
-use pocketmine\command\CommandSender;
+use DenielWorld\EzTiles\EzTiles;
 use pocketmine\command\Command;
-use pocketmine\item\Item;
+use pocketmine\command\CommandSender;
 use pocketmine\item\ItemFactory;
 use pocketmine\nbt\tag\StringTag;
-use DenielWorld\EzTiles\EzTiles;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as TF;
+use function file_exists;
+use function mkdir;
 
-class Main extends PluginBase {
+class Main extends PluginBase
+{
     public const VERSION = 4;
-    
-	private $cfg;
-	
-	public function onEnable(){
-		if(!file_exists($this->getDataFolder())){
-			@mkdir($this->getDataFolder());
-		} else if(!file_exists($this->getDataFolder() . "config.yml")){
-			$this->getLogger()->info("Config Not Found! Creating new config...");
-			$this->saveDefaultConfig();
-		}
-		$this->cfg = $this->getConfig()->getAll();
-		if($this->cfg["version"] < self::VERSION){
-			$this->getLogger()->error("Config Version is outdated! Please delete your current config file!");
-			$this->getServer()->getPluginManager()->disablePlugin($this);
-		}
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-		EzTiles::register($this);
-		EzTiles::init();
-	}
-	
-	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-	    
-	    if($command->getName() === "orespawner"){
-	        $typesArray = ["coal", "lapis", "iron", "gold", "diamond", "emerald", "redstone"];
-	        if(!$sender->hasPermission("orespawner.give")){
-	            $sender->sendMessage(TF::RED . "You do not have permission to use this command!");
-	            return false;
-	        }
-	        
-	        if(!isset($args[0])){
-	                $sender->sendMessage(TF::RED . "You must provide some arguments!");
-			return false;
-	        } else if(isset($args[2]) && !$this->getServer()->getPlayer($args[2])){
-			$sender->sendMessage(TF::RED . "You must provide a valid player!");
-		    	return false;
-	    	} else if(!isset($args[0]) || !in_array(strtolower($args[0]), $typesArray)){
-		    	$sender->sendMessage(TF::RED . "You must enter a valid ore type!");
-		    	return false;
-	    	} else if(isset($args[1]) && !is_numeric($args[1])){
-		    	$sender->sendMessage(TF::RED . "You must provide a valid amount!");
-		    	return false;
-	    	};
-	    	$player = isset($args[2]) ? $this->getServer()->getPlayer($args[2]) : $sender;
-	    	if (!$player instanceof Player) return false;
-		    $ore = strtolower($args[0]);
-	    	$amount = isset($args[1]) ? intval($args[1]) : 1;
-	    	$orespa = $this->createOreSpawner($ore, $amount);
-	        $player->getInventory()->addItem($orespa);
-	        return true;
-	    }
-	    return false;
-	}
-	
-	public function createOreSpawner(string $ore, int $amount) {
-	    $gen = $this->cfg["ore-generator-blocks"][$ore];
-	    $gencreated = ItemFactory::get(intval($gen), 0, $amount);
-	    $name = str_replace(["{ore}", "&"], [$ore, "§"], $this->cfg["ore-generators-name"] ?? "&a$ore ore spawner") ;
+
+    private $cfg;
+
+    public function onEnable()
+    {
+        if (!file_exists($this->getDataFolder())) {
+            @mkdir($this->getDataFolder());
+        } else if (!file_exists($this->getDataFolder() . "config.yml")) {
+            $this->getLogger()->info("Config Not Found! Creating new config...");
+            $this->saveDefaultConfig();
+        }
+        $this->cfg = $this->getConfig()->getAll();
+        if ($this->cfg["version"] < self::VERSION) {
+            $this->getLogger()->error("Config Version is outdated! Please delete your current config file!");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        EzTiles::register($this);
+        EzTiles::init();
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+    {
+
+        if ($command->getName() === "orespawner") {
+            $typesArray = ["coal", "lapis", "iron", "gold", "diamond", "emerald", "redstone"];
+            if (!$sender->hasPermission("orespawner.give")) {
+                $sender->sendMessage(TF::RED . "You do not have permission to use this command!");
+                return false;
+            }
+
+            if (!isset($args[0])) {
+                $sender->sendMessage(TF::RED . "You must provide some arguments!");
+                return false;
+            } else if (isset($args[2]) && !$this->getServer()->getPlayer($args[2])) {
+                $sender->sendMessage(TF::RED . "You must provide a valid player!");
+                return false;
+            } else if (!isset($args[0]) || !in_array(strtolower($args[0]), $typesArray)) {
+                $sender->sendMessage(TF::RED . "You must enter a valid ore type!");
+                return false;
+            } else if (isset($args[1]) && !is_numeric($args[1])) {
+                $sender->sendMessage(TF::RED . "You must provide a valid amount!");
+                return false;
+            }
+            $player = isset($args[2]) ? $this->getServer()->getPlayer($args[2]) : $sender;
+            if (!$player instanceof Player) return false;
+            $ore = strtolower($args[0]);
+            $amount = isset($args[1]) ? intval($args[1]) : 1;
+            $orespa = $this->createOreSpawner($ore, $amount);
+            $player->getInventory()->addItem($orespa);
+            return true;
+        }
+        return false;
+    }
+
+    public function createOreSpawner(string $ore, int $amount)
+    {
+        $gen = $this->cfg["ore-generator-blocks"][$ore];
+        $gencreated = ItemFactory::get(intval($gen), 0, $amount);
+        $name = str_replace(["{ore}", "&"], [$ore, "§"], $this->cfg["ore-generators-name"] ?? "&a$ore ore spawner");
         $gencreated->setCustomName($name);
         $lore = str_replace(["{ore}", "&"], [$ore, "§"], $this->cfg["ore-generators-lore"] ?? "Place it down, and ore blocks will spawn above it");
         $gencreated->setLore([$lore]);
         $gencreated->setNamedTagEntry(new StringTag("orespawner", "true"));
-	    return $gencreated;
-	}
+        return $gencreated;
+    }
 }
